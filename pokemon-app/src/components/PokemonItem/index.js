ed28e6532e68baa8582ad1f8e22472955dea5ed9
Taina Conde <tainabatistaconde@gmail.com>
2021-05-05 15:11:44 -0400

    feat: PokemonItem  - request api, render info
    Set up the base url for the pokemon image.
    Request that pokemon information from api.
    Render the image, name and pokemon types.

--- pokemon-app/src/components/PokemonItem/index.js
+++ pokemon-app/src/components/PokemonItem/index.js
@@ -1,6 +1,22 @@
+import {useEffect, useState} from 'react';
+import { P } from '../../api'
+
+const imagesBaseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world'
 const PokemonItem = (props) => {
+    const [pokemonStats, setPokemonStats] = useState({})
+    useEffect(()=> {
+        P.getPokemonByName(props.pokemon.name)
+        .then((result)=> {
+            setPokemonStats(result)
+        })
+    }, [])
     return (
         <li>
+            <img src = {`${imagesBaseUrl}/${pokemonStats.id}.svg`} alt = {props.pokemon.name}/>
+            <span>{pokemonStats.name}</span>
+            <div>{pokemonStats.types.map( index => (<div>
+                {index.type.name}
+            </div>))}</div>
             
         </li>
     );
