537f114d982a0dbd177c1d49664201754fae7c61
Taina Conde <tainabatistaconde@gmail.com>
2021-05-04 23:23:31 -0400

    feat: Service worker
    Download 'pokeapi-js-wrapper' service worker and
    serve it from the public folder of the project.

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,12 +1,17 @@
-
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { P } from './api/api'
 
 function App() {
+  useEffect(() => {
+    P.getPokemonByName("eevee")
+  .then(function(response) {
+    console.log(response)
+  })
+  }, [])
   return (
-    <div >
-      <header >
-       
-      </header>
+    <div>
+      <header></header>
     </div>
   );
 }
