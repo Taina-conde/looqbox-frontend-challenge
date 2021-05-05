a4a9f8cebae1826998a97438f42e64b31af5906c
Taina Conde <tainabatistaconde@gmail.com>
2021-05-05 13:36:46 -0400

    style: Searchbar - add bootstrap form classes

--- pokemon-app/src/components/Searchbar/index.js
+++ pokemon-app/src/components/Searchbar/index.js
@@ -1,23 +1,29 @@
 import { useState } from "react";
 import { BiSearchAlt2 } from "react-icons/bi";
-import './searchbar.css'
+import "./searchbar.css";
 
 const Searchbar = () => {
   const [query, setQuery] = useState("");
   const inputChangeHandler = (event) => {
-    setQuery(event.target.value)
-  }
+    setQuery(event.target.value);
+  };
   return (
-    <form className = ''>
-      <div className='input-wrapper'>
+    <form className="form-box">
+      <div className="input-group">
+        <span className="input-group-text" id="addon-search">
+          <BiSearchAlt2 size={20} />
+        </span>
+
         <input
+          className="form-control"
           type="search"
-          placeholder= "Search pokemon"
+          placeholder="Search pokemon"
           value={query}
           onChange={inputChangeHandler}
+          aria-describedby="addon-search"
+          aria-label="Search"
         />
       </div>
-      <BiSearchAlt2/>
     </form>
   );
 };
