55bacce4a89ce9445cae0a0c066c1ca9e2b07663
Taina Conde <tainabatistaconde@gmail.com>
2021-05-05 14:16:30 -0400

    feat: App - when App initially renders, load a list of pokemons
    Use useEffect hook to make a request to pokeapi to
    load a list of 20 pokemons. Use useState hook to
    pass that data to the other components like HomeView.

--- pokemon-app/src/components/App/index.js
+++ pokemon-app/src/components/App/index.js
@@ -1,19 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { P } from '../../api'
-import HomeView from '../HomeView';
+import { P } from "../../api";
+import HomeView from "../HomeView";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
   useEffect(() => {
-    P.getPokemonByName("eevee")
-  .then(function(response) {
-    console.log(response)
-  })
-  }, [])
+    const interval = {
+      offset: 0,
+      limit: 20,
+    };
+    P.getPokemonsList(interval).then(function (response) {
+      setPokemons(response.results);
+    });
+  }, []);
   return (
     <div>
       <header></header>
-      <HomeView/>
+      <HomeView pokemons = {pokemons}/>
     </div>
   );
 }
--- pokemon-app/src/components/HomeView/index.js
+++ pokemon-app/src/components/HomeView/index.js
@@ -1,9 +1,12 @@
-import Searchbar from '../Searchbar'
-import PokemonList from '../PokemonList'
+import Searchbar from "../Searchbar";
+import PokemonList from "../PokemonList";
+
 const HomeView = () => {
-  return <div>
-      <Searchbar/>
-      <PokemonList/>
-  </div>;
+  return (
+    <div>
+      <Searchbar />
+      <PokemonList />
+    </div>
+  );
 };
 export default HomeView;
