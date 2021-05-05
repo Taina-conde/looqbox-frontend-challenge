ae6835be4273fb33b406abfe2656c882200e70f9
Taina Conde <tainabatistaconde@gmail.com>
2021-05-05 13:51:58 -0400

    feat: HomeView - import and return PokemonList component in HomeView

--- pokemon-app/src/components/HomeView/index.js
+++ pokemon-app/src/components/HomeView/index.js
@@ -1,7 +1,9 @@
 import Searchbar from '../Searchbar'
+import PokemonList from '../PokemonList'
 const HomeView = () => {
   return <div>
       <Searchbar/>
+      <PokemonList/>
   </div>;
 };
 export default HomeView;
