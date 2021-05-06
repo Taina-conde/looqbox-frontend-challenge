afbef703bc3042bc93132ed710f6c22fcf5b420d
Taina Conde <tainabatistaconde@gmail.com>
2021-05-05 22:35:11 -0400

    feat: BackToHome - create router Link to HomeView
    Create component folder and index file.
    Render component in NoMatch component.

--- pokemon-app/src/components/NoMatch/index.js
+++ pokemon-app/src/components/NoMatch/index.js
@@ -1,10 +1,12 @@
 import { useLocation } from 'react-router-dom';
+import BackToHome from '../BackToHome';
 const NoMatch = () => {
     let location = useLocation();
     return(
         <div>
-            <span>404</span>
-            <span>The page you are looking for <strong>{location.pathname}</strong> does not exist</span>
+            <div>404</div>
+            <div>The page you are looking for <strong>{location.pathname}</strong> does not exist</div>
+            <BackToHome/>
         </div>
     )
 }
