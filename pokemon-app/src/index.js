136a0d09bedfaa702b048cabbfd90ee5f5e922fc
Taina Conde <tainabatistaconde@gmail.com>
2021-05-07 13:56:14 -0400

    feat: Pass reducer as argument in createStore

--- pokemon-app/src/index.js
+++ pokemon-app/src/index.js
@@ -5,8 +5,9 @@ import App from "./components/App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import reducer from "./redux/reducers"
 
-const store = createStore();
+const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store = {store}>
