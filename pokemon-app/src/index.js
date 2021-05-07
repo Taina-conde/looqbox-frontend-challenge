10cdf6087751c931b5c127574c033b9a2ac58536
Taina Conde <tainabatistaconde@gmail.com>
2021-05-07 13:18:43 -0400

    feat: Install redux, react-redux and create the store in main  index.js

--- pokemon-app/src/index.js
+++ pokemon-app/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore();
 
 ReactDOM.render(
- <Router>
-   <App />
- </Router>
-  ,
-  document.getElementById('root')
+  <Provider store = {store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+
+  document.getElementById("root")
 );
-
-
