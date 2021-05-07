317b9563ef4090beae498bc147f7ee106f412404
Taina Conde <tainabatistaconde@gmail.com>
2021-05-07 14:17:10 -0400

    feat: Actions - create addPokemonStats action

--- pokemon-app/src/redux/actions/index.js
+++ pokemon-app/src/redux/actions/index.js
@@ -1,9 +1,17 @@
 export const RECEIVE_POKEMON_LIST = 'RECEIVE_POKEMON_LIST';
+export const ADD_POKEMON_STATS = 'ADD_POKEMON_STATS'
 
 
 export function receiveList(pokemons) {
     return {
         type: RECEIVE_POKEMON_LIST,
         pokemons
+    }
+}
+export function addPokemonStats(name, stats) {
+    return {
+        type: ADD_POKEMON_STATS,
+        name,
+        stats
     }
 }
